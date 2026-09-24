2e20e2d4000751c3fae319d01720b7568f8b768e
10573LLC <wflack@anconisonpmg.com>
2026-09-23 21:44:12 -0700

    Allow HTTPS to AIRS load balancer

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -56,6 +56,7 @@ resource "aws_security_group" "alb" {
   name_prefix = "${var.app_name}-alb-"
   vpc_id = aws_vpc.main.id
   ingress { from_port = 80; to_port = 80; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
+  ingress { from_port = 443; to_port = 443; protocol = "tcp"; cidr_blocks = ["0.0.0.0/0"] }
   egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
   lifecycle { create_before_destroy = true }
 }

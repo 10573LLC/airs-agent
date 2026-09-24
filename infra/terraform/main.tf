ab00f356180c6c367a7c4957727b40d81f76d4e5
10573LLC <wflack@anconisonpmg.com>
2026-09-24 06:34:59 -0700

    Break ALB and app security group Terraform cycle

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -130,16 +130,17 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port       = 3000
-    to_port         = 3000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.app.id]
-  }
-
   lifecycle {
     create_before_destroy = true
   }
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_to_app" {
+  security_group_id            = aws_security_group.alb.id
+  referenced_security_group_id = aws_security_group.app.id
+  from_port                    = 3000
+  to_port                      = 3000
+  ip_protocol                  = "tcp"
 }
 
 resource "aws_security_group" "app" {
